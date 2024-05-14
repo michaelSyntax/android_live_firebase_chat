77e1195b2ebce80961c36afdeac6b6b05decadc7
michaelSyntax <michael.karbe@syntax-institut.de>
2024-05-14 21:17:39 +0200

    Update Adapter with basic impl.

--- app/src/main/java/com/example/android_live_firebase_chat/adapter/ChatAdapter.kt
+++ app/src/main/java/com/example/android_live_firebase_chat/adapter/ChatAdapter.kt
@@ -1,4 +1,41 @@
 package com.example.android_live_firebase_chat.adapter
 
-class ChatAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.android_live_firebase_chat.databinding.ItemChatInBinding
+import com.example.android_live_firebase_chat.databinding.ItemChatOutBinding
+import com.example.android_live_firebase_chat.model.Message
+
+class ChatAdapter(
+    private val dataset: List<Message>,
+    private val currentUserId: String
+): RecyclerView.Adapter<ViewHolder>() {
+
+    private val chatInType = 1
+    private val chatOutType = 2
+
+    override fun getItemViewType(position: Int): Int {
+        val chatItem = dataset[position]
+        return if (chatItem.sender == currentUserId) {
+            chatOutType
+        } else {
+            chatInType
+        }
+    }
+
+    inner class ChatInViewHolder(val binding: ItemChatInBinding): RecyclerView.ViewHolder(binding.root)
+    inner class ChatOutViewHolder(val binding: ItemChatOutBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/com/example/android_live_firebase_chat/adapter/UserAdapter.kt
+++ app/src/main/java/com/example/android_live_firebase_chat/adapter/UserAdapter.kt
@@ -1,4 +1,27 @@
 package com.example.android_live_firebase_chat.adapter
 
-class UserAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android_live_firebase_chat.MainViewModel
+import com.example.android_live_firebase_chat.databinding.ItemUserBinding
+import com.example.android_live_firebase_chat.model.Profile
+
+class UserAdapter(
+    private val dataset: List<Profile>,
+    private val viewModel: MainViewModel
+): RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+    inner class UserViewHolder(val binding: ItemUserBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
