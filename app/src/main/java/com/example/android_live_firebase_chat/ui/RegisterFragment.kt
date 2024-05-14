ff914b65ef0fe9baaa015d58fb9e0eeba2fdf5e3
michaelSyntax <michael.karbe@syntax-institut.de>
2024-05-14 20:19:30 +0200

    Add Blank Fragments with layout files.

---
+++ app/src/main/java/com/example/android_live_firebase_chat/ui/RegisterFragment.kt
@@ -0,0 +1,24 @@
+package com.example.android_live_firebase_chat.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.android_live_firebase_chat.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [RegisterFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class RegisterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+}
